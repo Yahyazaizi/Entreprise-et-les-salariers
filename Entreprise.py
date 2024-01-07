dda7b6336bc0fa93d36d8c05e0f2fc2262ea944b
yahya zaizi <146640218+Yahyazaizi@users.noreply.github.com>
2024-01-08 00:14:25 +0100

    Update Entreprise.py

--- Entreprise.py
+++ Entreprise.py
@@ -1,12 +1,5 @@
 from a import salariers
-class Cashier(salariers):  
-    def _init_(self, name='yahya', number=0, marital_status='single', address='ISGI', base_salary=0):
-       
-        super()._init_(name, number, marital_status, address, base_salary)
 
-    
-    def salary(self):
-        return self.get_base_salary
 class Cashier(salariers):
     def _init_(self, name='yahya', number=0, marital_status='single', address='ISGI', base_salary=0):
         
